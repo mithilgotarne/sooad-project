0b7043de9a09739be4ffd8ccc3958ec1736c0b68
mithilgotarne <mithilgotarne@gmail.com>
2016-08-07 20:15:58 +0530

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,51 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp',['ngRoute'])
+    
+    .controller('overviewCtrl', function($scope) {
+
+        $scope.about = "need ipsum! very design, many text. many word! oh my full. go swag, very amet. " +
+            "plz ipsum! wow. oh my word! very consectetur, much lorem. i iz cute?. wow! very consectetur, " +
+            "wow! so ipsum! very full. much lorem. need lorem. plz ipsum! oh my full. such sit. yes master doge,";
+
+        $scope.academics = [
+            {
+                course:"S.S.C.",
+                institute:"S.E.S. High School",
+                city:"Thane",
+                board:"Maharashtra",
+                yop:"2012",
+                marks:"91.45%"
+            },
+            {
+                course:"H.S.C.",
+                institute:"S.E.S. Jr. College",
+                city:"Thane",
+                board:"Maharashtra",
+                yop:"2014",
+                marks:"89.38%"
+            },
+            {
+                course:"Such Course",
+                institute:"Much Institution",
+                city:"Wow City",
+                board:"Such Board",
+                yop:"Much year",
+                marks:"Wow marks"
+            }
+        ];
+
+    })
+
+    .config(function ($locationProvider, $routeProvider) {
+
+        $routeProvider
+            .when('/', {
+                templateUrl : 'html/overview.html',
+                controller : 'overviewCtrl'
+            });
+
+
+        $locationProvider.html5Mode(true);
+    });
