f2ae517af1747584618a63a194146b5163f7813f
mithishri <mithilgotarne@gmail.com>
2016-11-10 00:14:08 +0530

    last commit

--- app/app.js
+++ app/app.js
@@ -4,12 +4,17 @@
 angular.module('myApp', ['ngRoute', 'easypiechart', 'ngMaterial', 'firebase'])
 
 
-    .run(function ($rootScope, $location) {
+    .run(function ($rootScope, $location, $firebaseObject) {
 
         $rootScope.isLoggedIn = localStorage.getItem('isLoggedIn') || false;
 
-        if (!$rootScope.isLoggedIn)
+        if ($rootScope.isLoggedIn) {
+
+            var ref = firebase.database().ref().child("users").child(localStorage.getItem("username"));
+            $rootScope.basic = $firebaseObject(ref.child("profile/basic"));
+        } else {
             $location.path('/login');
+        }
 
         $rootScope.titles = {
             '/overview': 'Profile Overview',
@@ -37,7 +42,7 @@ angular.module('myApp', ['ngRoute', 'easypiechart', 'ngMaterial', 'firebase'])
 
     })
 
-    .controller('loginCtrl', function ($rootScope, $scope, $location) {
+    .controller('loginCtrl', function ($rootScope, $scope, $location, $firebaseObject) {
 
         $scope.username = '';
         $scope.password = '';
@@ -46,131 +51,49 @@ angular.module('myApp', ['ngRoute', 'easypiechart', 'ngMaterial', 'firebase'])
 
         $scope.login = function () {
 
-            $rootScope.isLoggedIn = true;
-            $location.path('/overview');
-            localStorage.setItem('isLoggedIn', true);
+            var ref = firebase.database().ref().child("users").child($scope.username);
+
+            var user = $firebaseObject(ref);
+
+            user.$loaded().then(function () {
+
+                if ($scope.password == user.pass) {
+
+
+                    $rootScope.isLoggedIn = true;
+                    $location.path('/overview');
+                    localStorage.setItem('isLoggedIn', true);
+                    localStorage.setItem('username', user.id);
+
+                }
+
+            });
+
         }
 
     })
 
-    .controller('overviewCtrl', function ($scope) {
+    .controller('overviewCtrl', function ($scope, $firebaseObject, $firebaseArray) {
 
-        $scope.about = "need ipsum! very design, many text. many word! oh my full. go swag, very amet. " +
-            "plz ipsum! wow. oh my word! very consectetur, much lorem. i iz cute?. wow! very consectetur, " +
-            "wow! so ipsum! very full. much lorem. need lorem. plz ipsum! oh my full. such sit. yes master doge,";
+        var ref = firebase.database().ref().child("users").child(localStorage.getItem('username'));
 
-        $scope.academics = [
-            {
-                course: "S.S.C.",
-                institute: "S.E.S. High School",
-                city: "Thane",
-                board: "Maharashtra",
-                yop: "2012",
-                marks: "91.45%"
-            },
-            {
-                course: "H.S.C.",
-                institute: "S.E.S. Jr. College",
-                city: "Thane",
-                board: "Maharashtra",
-                yop: "2014",
-                marks: "89.38%"
-            },
-            {
-                course: "Such Course",
-                institute: "Much Institution",
-                city: "Wow City",
-                board: "Such Board",
-                yop: "Much year",
-                marks: "Wow marks"
-            }
-        ];
+        $scope.profile = $firebaseObject(ref.child("profile/basic"));
 
-        $scope.projects = [
-            {
-                title: "Popular",
-                url: "https://github.com/mithilgotarne/Popular"
-            },
-            {
-                title: "E-Cell, SPIT",
-                url: "https://ecellsp.org"
-            },
-            {
-                title: "Virtual Stock Market",
-                url: "https://github.com/mithilgotarne/VSM2"
-            }
-        ];
+        $scope.about = $firebaseObject(ref.child("profile/about"));
 
-        $scope.skills = [
-            {
-                title: "Aptitude",
-                percent: 70,
-                options: {
-                    animate: {
-                        duration: 2000,
-                        enabled: true
-                    },
-                    barColor: '#ACEC00',
-                    trackColor: '#EAFABF',
-                    scaleColor: false,
-                    lineWidth: 20,
-                    lineCap: 'round',
-                    size: 200
-                }
-            },
-            {
-                title: "Soft Skills",
-                percent: 60,
-                options: {
-                    animate: {
-                        duration: 2000,
-                        enabled: true
-                    },
-                    barColor: '#00BBD6',
-                    trackColor: '#BFEEF5',
-                    scaleColor: false,
-                    lineWidth: 20,
-                    lineCap: 'round',
-                    size: 200
-                }
-            },
-            {
-                title: "Technical",
-                percent: 80,
-                options: {
-                    animate: {
-                        duration: 2000,
-                        enabled: true
-                    },
-                    barColor: '#BA65C9',
-                    trackColor: '#EED8F1',
-                    scaleColor: false,
-                    lineWidth: 20,
-                    lineCap: 'round',
-                    size: 200
-                }
-            },
-            {
-                title: "Programming",
-                percent: 50,
-                options: {
-                    animate: {
-                        duration: 2000,
-                        enabled: true
-                    },
-                    barColor: '#EF3C79',
-                    trackColor: '#FBCEDD',
-                    scaleColor: false,
-                    lineWidth: 20,
-                    lineCap: 'round',
-                    size: 200
-                }
-            }
-        ]
+        $scope.academics = $firebaseArray(ref.child("academics"));
+
+        $scope.overview = $firebaseObject(ref.child("overview"));
+
+        $scope.projects = $firebaseArray(ref.child("projects"));
+
+        $scope.skills = $firebaseArray(ref.child("resume/skills"));
 
     })
 
     .controller('profileCtrl', function ($scope, $firebaseObject) {
+
+        var ref = firebase.database().ref().child("users").child(localStorage.getItem('username')).child("profile");
 
         $scope.data = {
             selectedIndex: 1
@@ -182,77 +105,40 @@ angular.module('myApp', ['ngRoute', 'easypiechart', 'ngMaterial', 'firebase'])
             $scope.data.selectedIndex = Math.max($scope.data.selectedIndex - 1, 0);
         };
 
-        $scope.basic = {
-            name: 'Name Surname',
-            roll_no: '2014130019',
-            gender: 'Male',
-            pwd: false,
-            dob: new Date('1996-09-20'),
-            aadhar: '123456789',
+        $scope.genders = ['Male', 'Female', 'Other'];
 
-            genders: ['Male', 'Female', 'Other']
-        };
+        $firebaseObject(ref.child("basic")).$bindTo($scope, "basic");
+        $firebaseObject(ref.child("contact")).$bindTo($scope, "contact");
+        $firebaseObject(ref.child("guardian")).$bindTo($scope, "guardian");
+        $firebaseObject(ref.child("about")).$bindTo($scope, "about");
 
-        $scope.contact = {
-            email: 'example@example.com',
-            address: '3665 Quaking Walk, Elmo, Missouri, 65301-3520, US',
-            phone: '(314) 322-8789',
-            landline: '496-21-1452'
-        };
+        var date = new $firebaseObject(ref.child("basic").child("dob"));
 
-        var ref = firebase.database().ref().child("users/user1/profile");
-        var syncObject = $firebaseObject(ref);
-        syncObject.$bindTo($scope, "contact");
+        $scope.dob = new Date();
 
-
-
-        $scope.guardian = {
-
-            father: {
-                name: '',
-                phone: '',
-                occupation: ''
-            },
-
-            mother: {
-                name: '',
-                phone: '',
-                occupation: ''
-            }
-        };
-
-        $scope.aboutMe = 'need ipsum! very design, many text. many word! oh my full. go swag, very amet. plz ipsum! wow. oh my word! very consectetur, much lorem. i iz cute?. wow! very consectetur, wow! so ipsum! very full. much lorem. need lorem. plz ipsum! oh my full. such sit. yes master doge,';
+        date.$loaded().then(function () {
+            $scope.dob = new Date(date.$value);
+        });
 
 
     })
 
-    .controller('resumeCtrl', function ($scope) {
+    .controller('resumeCtrl', function ($scope, $firebaseArray, $firebaseObject) {
 
-        $scope.strengths = [
-            {
-                name: 'Strength 1',
-                desc: 'Ubi est velox gluten?Pol, a bene exsul, bi-color imber!A falsis, clabulare magnum acipenser.'
-            }
-        ];
+        var ref = firebase.database().ref().child("users").child(localStorage.getItem('username')).child("resume");
 
-        $scope.rmStrength = function (index) {
 
-            $scope.strengths.splice(index, 1);
-
-        };
+        $scope.strengths = $firebaseArray(ref.child("strengths"));
 
         $scope.addStrength = function () {
-            $scope.strengths.push({name: '', desc: ''});
+            $scope.strengths.$add({name: '', desc: ''});
         };
 
-        $scope.hobbies = ['Interset 1', 'Hobby 1'];
+        $scope.hobbies = $firebaseArray(ref.child("hobbies"));
 
-        $scope.rmHobby = function (index) {
-            $scope.hobbies.splice(index, 1);
-        };
 
         $scope.addHobby = function () {
-            $scope.hobbies.push('');
+            $scope.hobbies.$add({name: ''});
         };
 
 
@@ -261,63 +147,24 @@ angular.module('myApp', ['ngRoute', 'easypiechart', 'ngMaterial', 'firebase'])
             back: 0
         };
 
+        $firebaseObject(ref.child("agg")).$bindTo($scope, "agg");
 
-        $scope.skills = [
-            {
-                title: "Aptitude",
-                percent: 70
-            },
-            {
-                title: "Soft Skills",
-                percent: 60
-            },
-            {
-                title: "Technical",
-                percent: 80
-            },
-            {
-                title: "Programming",
-                percent: 50
-            }
-        ];
+
+        $scope.skills = $firebaseArray(ref.child("skills"));
 
 
     })
 
-    .controller('portfolioCtrl', function ($scope) {
+    .controller('portfolioCtrl', function ($scope, $firebaseArray) {
 
-        $scope.portfolios = [
 
-            {
-                img: 'images/washedout.png',
-                title: 'Project 1',
-                desc: 'Vita de azureus cottaraicus tam.Aonides salvus tabes est.Nunquam examinare boreas.Cum bulla experimentum, omnes vitaes resuscitabo fidelis, emeritis clabularees.',
-                url: '#'
-            },
-            {
-                img: 'images/washedout.png',
-                title: 'Project 2',
-                desc: 'Vita de azureus cottaraicus tam.Aonides salvus tabes est.Nunquam examinare boreas.Cum bulla experimentum, omnes vitaes resuscitabo fidelis, emeritis clabularees.',
-                url: '#'
-            },
-            {
-                img: 'images/washedout.png',
-                title: 'Project 3',
-                desc: 'Vita de azureus cottaraicus tam.Aonides salvus tabes est.Nunquam examinare boreas.Cum bulla experimentum, omnes vitaes resuscitabo fidelis, emeritis clabularees.',
-                url: '#'
-            },
-            {
-                img: 'images/washedout.png',
-                title: 'Project 4',
-                desc: 'Vita de azureus cottaraicus tam.Aonides salvus tabes est.Nunquam examinare boreas.Cum bulla experimentum, omnes vitaes resuscitabo fidelis, emeritis clabularees.',
-                url: '#'
-            }
+        var ref = firebase.database().ref().child("users").child(localStorage.getItem('username'));
 
-        ];
+        $scope.portfolios = $firebaseArray(ref.child("projects"));
 
         $scope.addPortfolio = function () {
 
-            $scope.portfolios.push(
+            $scope.portfolios.$add(
                 {
                     img: 'images/washedout.png',
                     title: 'Project ' + ($scope.portfolios.length + 1),
@@ -328,9 +175,6 @@ angular.module('myApp', ['ngRoute', 'easypiechart', 'ngMaterial', 'firebase'])
 
         };
 
-        $scope.rmPortfolio = function (index) {
-            $scope.portfolios.splice(index, 1);
-        };
 
     })
 
